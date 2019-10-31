cdebff1751df48f9a3235d4f2c72d29f211ce49a
Xin Tong <xint5@illinois.edu>
2019-10-31 17:50:12 -0500

    Modify user update permissions

--- users/permissions.py
+++ users/permissions.py
@@ -1,7 +1,8 @@
 from rest_framework import permissions
 
-class IsOwner(permissions.BasePermission):
-  ''' Custom permission to only allow owners of an object to edit it. '''
+class canUpdate(permissions.BasePermission):
+  ''' Custom permission to only allow users to update their own CustomUser record. '''
 
   def has_object_permission(self, request, view, obj):
-    return obj.username == request.user.username
+    return obj.username == request.user.username and \
+      ('username' not in request.data or obj.username == request.data['username'])
--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ from rest_framework.decorators import api_view, permission_classes
 
 from .serializers import UserSerializer
 from .models import CustomUser
-from .permissions import IsOwner
+from .permissions import canUpdate
 
 class UserRegisterView(generics.CreateAPIView):
   ''' View for user registration '''
@@ -19,10 +19,8 @@ class UserRegisterView(generics.CreateAPIView):
 
 class UserUpdateView(generics.RetrieveUpdateDestroyAPIView):
   queryset = CustomUser.objects.all()
-  permission_classes = [ IsOwner ]
+  permission_classes = [ canUpdate ]
   serializer_class = UserSerializer
-
-  # TODO forbit updating other's record
 
 class TokenDestroyView(views.APIView):
   ''' View for user token expiration / logout '''

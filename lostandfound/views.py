b4365b3bd8e5f3aa364d4de716487eb961b2f542
Xin Tong <xint5@illinois.edu>
2019-12-03 15:49:12 -0600

    Update img id/file parsing & utils

--- lostandfound/views.py
+++ lostandfound/views.py
@@ -5,8 +5,8 @@ from rest_framework.decorators import api_view
 from mongoengine import *
 
 from .models import Item
-from .utils import encode_base64, decode_base64, get_image_filename, get_id_from_image_filename
 from .similarimages import get_similar_image
+from . import utils
 
 
 @api_view(['POST'])
@@ -27,20 +27,18 @@ def addItemView(request):
     item.user = request.user.username
     item.save()
 
-    decode_base64(get_image_filename(str(item.id), item.is_lost), img)
+    utils.decode_base64(utils.get_image_filename(str(item.id), item.is_lost), img)
 
     if item.is_lost: # find the current best matching found images
-      similar_found_imgs = get_similar_image(str(item.id), "found/", K=3) # [[score, filename], []]
-      for img_info in similar_found_imgs:
-        img_info[1] = get_id_from_image_filename(img_info[1])
-      similar_found_imgs.sort(key=lambda x:x[0])
-      item.matched_info = similar_found_imgs
+      similar_found_img_ids = get_similar_image(str(item.id), "found/", K=3) # [[score, filename], []]
+      similar_found_img_ids.sort(key=lambda x:x[0])
+      item.matched_info = similar_found_img_ids
 
     else: # refresh lost matching when adding found image
-      similar_lost_imgs = get_similar_image(str(item.id), "lost/", K=float("inf"))
+      similar_lost_img_ids = get_similar_image(str(item.id), "lost/", K=float("inf"))
 
-      for score, img_file in similar_lost_imgs:
-        lost_item = Item.objects(id=get_id_from_image_filename(img_file), is_lost=True)[0]
+      for score, img_id in similar_lost_img_ids:
+        lost_item = Item.objects(id=img_id, is_lost=True)[0]
         if lost_item.matched_info:
           fscore, _ = lost_item.matched_info[0]
           if score > fscore:
@@ -48,6 +46,7 @@ def addItemView(request):
             lost_item.matched_info.sort(key=lambda x:x[0])
         elif score > 0.6:
           lost_item.matched_info.append([score, item.id])
+
     item.save()
     print("Added item: ", item.id, item)
 
@@ -76,7 +75,7 @@ def updateItemView(request):
     if 'image' in data:
       img = data['image']
       del data['image']
-      decode_base64(get_image_filename(str(item.id), item.is_lost), img)
+      utils.decode_base64(utils.get_image_filename(str(item.id), item.is_lost), img)
 
     for k, v in data.items():
       item[k] = v
@@ -105,11 +104,7 @@ def deleteItemView(request):
       )
     item = item[0]
 
-    try:
-      os.remove(get_image_filename(str(item.id), item.is_lost))
-    except FileNotFoundError as e:
-      print(e)
-
+    utils.remove_image_file(item.id, item.is_lost)
     item.delete()
   except (KeyError, IndexError, ValidationError, FieldDoesNotExist) as e:
     return Response(
@@ -126,7 +121,7 @@ def getUserLostItems(request):
   username = request.user.username
   items = Item.objects(user=username, is_lost=True)
   objects = [item.to_json() for item in items]
-  images = [encode_base64(get_image_filename(str(item.id), item.is_lost)) for item in items]
+  images = [utils.encode_base64(utils.get_image_filename(str(item.id), item.is_lost)) for item in items]
   return Response(
     data={
       'lost_items': objects,
@@ -139,7 +134,7 @@ def getUserFoundItems(request):
   username = request.user.username
   items = Item.objects(user=username, is_lost=False)
   objects = [item.to_json() for item in items]
-  images = [encode_base64(get_image_filename(str(item.id), item.is_lost)) for item in items]
+  images = [utils.encode_base64(utils.get_image_filename(str(item.id), item.is_lost)) for item in items]
   return Response(
     data={
       'found_items': objects,
@@ -164,8 +159,8 @@ def getMatchedFoundItems(request):
     matched_items, matched_images = [], []
     for _, matched_id in lost_item.matched_info:
       try:
-        matched_items.append(Item.objects(id=matched_id)[0])
-        matched_images = encode_base64(get_image_filename(matched_id, is_lost=False))
+        matched_items.append(Item.objects(id=matched_id)[0].to_json())
+        matched_images.append(utils.encode_base64(utils.get_image_filename(matched_id, is_lost=False)))
       except:
         print("invalid match id: ", matched_id)
         continue
